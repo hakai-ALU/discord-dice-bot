5c9394e3ff602c95adc7101f10eec68701a028d1
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-06-21 21:47:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,14 +6,14 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
+@bot.command(name="こんにちは")
+async def hello(ctx):
+    await ctx.send(f"どうも、{ctx.message.author.name}さん！")
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.command(name="さようなら")
+async def goodbye(ctx):
+    await ctx.send(f"じゃあね、{ctx.message.author.name}さん！")
 
 
 bot.run(token)
