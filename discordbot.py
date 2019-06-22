855a805805113d12a5178803f339dd905b889ae9
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-06-22 09:38:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -15,10 +15,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-@client.event
-async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-        await message.channel.send(reply) # 返信メッセージを送信
 
 bot.run(token)
