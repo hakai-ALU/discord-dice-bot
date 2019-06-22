52c0e323244074ee1f91e6568c4d8fbbf3b1ca9d
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-06-22 09:53:15 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,18 +2,25 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
     
     
 bot.run(token)
