7ca9441a9f7999ea2894f29ed6d34e455f6d33aa
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-06-22 17:55:24 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
 client = discord.Client()
 
-メッセージ受信時に動作する処理
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
