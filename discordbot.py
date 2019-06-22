d754786a6be6e32dc891d8aa2b24fc1f1d7b547c
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-06-22 09:50:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,18 +2,18 @@ from discord.ext import commands
 import os
 import traceback
 
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-client = discord.Client()
 
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == 'おはよう':
-        await message.channel.send('おはようございます！！')
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
     
     
 bot.run(token)
