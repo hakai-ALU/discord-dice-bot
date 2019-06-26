c7ac7d0aa1ec25beabdfdddff3f022a7c78d0d72
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-06-26 21:44:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,16 +14,17 @@ async def on_ready():
 
 # メッセージ受信時に動作する処理
 @client.event
-   async def on_message(message):
+async def on_message(message):
  # メッセージ送信者がBotだった場合は無視する
-    　　if message.author.bot:
-        　　return
-        if all(s in message.content for s in['何日？']):
-            date = datetime.datetime.now()
-            await message.channel.send(f'今日は{date.year}年{date.month}月{date.day}日です！')    
-        if all(s in message.content for s in ['何時？']):
-            date = datetime.datetime.now()
-            await message.channel.send(f'今は{date.hour}時{date.minute}分{date.second}秒だよ！')
+    if message.author.bot:
+        return
+ #年月日
+    if all(s in message.content for s in['何日？']):
+        date = datetime.datetime.now()
+        await message.channel.send(f'今日は{date.year}年{date.month}月{date.day}日です！')    
+    if all(s in message.content for s in ['何時？']):
+        date = datetime.datetime.now()
+        await message.channel.send(f'今は{date.hour}時{date.minute}分{date.second}秒だよ！')
 
 client.run(TOKEN)
 
