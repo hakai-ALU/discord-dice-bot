2735f004d3bd8c44f211662708264e5f32fec6f4
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-06-29 20:56:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,13 +26,42 @@ async def on_message(message):
     if all(s in message.content for s in ['何時？']):
         date = datetime.datetime.now()
         await message.channel.send(f'今は{date.hour}時{date.minute}分{date.second}秒だよ！')
-　　if message.content == 'おみくじ':
+@client.event
+async def on_ready():
+    """起動時に通知してくれる処理"""
+    print('ログインしました')
+    print(client.user.name)  # ボットの名前
+    print(client.user.id)  # ボットのID
+    print(discord.__version__)  # discord.pyのバージョン
+    print('------')
+
+@client.event
+async def on_message(message):
+    """メッセージを処理"""
+    if message.author.bot:  # ボットのメッセージをハネる
+        return
+
+    if message.content == "!眠たい":
+        # チャンネルへメッセージを送信
+        await message.channel.send(f"{message.author.mention}さん 寝ましょう")  # f文字列（フォーマット済み文字列リテラル）
+
+    elif message.content == "!投票":
+        # リアクションアイコンを付けたい
+        q = await message.channel.send("あなたは右利きですか？")
+        [await q.add_reaction(i) for i in ('⭕', '❌')]  # for文の内包表記
+
+    elif message.content == "!おみくじ":
         # Embedを使ったメッセージ送信 と ランダムで要素を選択
-        embed = discord.Embed(title='おみくじ', description=f'{message.author.mention}さんの今日の運勢は ',
+        embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢は！",
                               color=0x2ECC69)
         embed.set_thumbnail(url=message.author.avatar_url)
-        embed.add_field(name='[運勢] ', value=random.choice(('大吉', '吉', '凶', '大凶')), inline=False)
+        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '吉', '凶', '大凶')), inline=False)
         await message.channel.send(embed=embed)
+
+    elif message.content == "!ダイレクトメッセージ":
+        # ダイレクトメッセージ送信
+        dm = await message.author.create_dm()
+        await dm.send(f"{message.author.mention}さんにダイレクトメッセージ")
 
 client.run(TOKEN)
 
