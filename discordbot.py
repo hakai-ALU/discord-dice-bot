aeb891347e137e79ae305a8b5daaf6a1d391656e
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-06-29 20:47:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,6 +25,13 @@ async def on_message(message):
     if all(s in message.content for s in ['何時？']):
         date = datetime.datetime.now()
         await message.channel.send(f'今は{date.hour}時{date.minute}分{date.second}秒だよ！')
+　　if message.content == "!おみくじ":
+        # Embedを使ったメッセージ送信 と ランダムで要素を選択
+        embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢は！",
+                              color=0x2ECC69)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '吉', '凶', '大凶')), inline=False)
+        await message.channel.send(embed=embed)
 
 client.run(TOKEN)
 
