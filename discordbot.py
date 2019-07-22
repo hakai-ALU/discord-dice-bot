000250f2c71db0667d1a3af5dc8c252da9cd3195
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-07-22 21:23:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,23 @@
 import discord 
 import os
+from discord.ext import tasks
+from datetime import datetime
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
-
+CHANNEL_ID = 587658526013390859 #チャンネルID
+# 接続に必要なオブジェクトを生成
 client = discord.Client()
+
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '09:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')  
+
+#ループ処理実行
+loop.start()
 
 client.run(TOKEN)
