f0c050e15290c3c2e4c808286049cb9a13762c89
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-07-25 20:58:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,7 +27,7 @@ async def on_message(message):
         embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢は！",
                               color=0x2ECC69)
         embed.set_thumbnail(url=message.author.avatar_url)
-        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '吉', '凶', '大凶')), inline=False)
+        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '中吉', '小吉', '吉', '半吉', '末吉', '末小吉', '凶', '小凶', '半凶', '末凶', '大凶')), inline=False)
         await message.channel.send(embed=embed)
 
 # 60秒に一回ループ
