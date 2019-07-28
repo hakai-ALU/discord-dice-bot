9bb373bedc4c6acc5510a308dcdfe52e0dea11ab
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-07-28 20:25:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import tasks
 from datetime import datetime
 import random
+import re
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 CHANNEL_ID = 587658526013390859 #チャンネルID
@@ -31,6 +32,32 @@ async def on_message(message):
         embed.set_thumbnail(url=message.author.avatar_url)
         embed.add_field(name="[運勢] ", value=random.choice(('大吉', '中吉', '小吉', '吉', '半吉', '末吉', '末小吉', '凶', '小凶', '半凶', '末凶', '大凶')), inline=False)
         await client.get_channel(CHANNEL_ID3).send(embed=embed)
+
+ #御籤 
+    #「翠、おみくじ引かせて！」って言うとおみくじ引く
+    if message.content == 'おみくじ' or message.content == '運勢':
+        prob = random.random()
+    
+        if prob < 0.3:
+            await message.channel.send('凶です……外出を控えることをオススメします(  ･᷄ὢ･᷅  )')
+        
+        elif prob < 0.65:
+            await message.channel.send('吉です！何かいい事があるかもですね！')
+        
+        elif prob < 0.71:
+            await message.channel.send('末吉……どれくらい運がいいんでしょうね？•́ω•̀)?')
+        
+        elif prob < 0.76:
+            await message.channel.send('半吉は吉の半分、つまり運がいいのです！')
+        
+        elif prob < 0.80:
+            await message.channel.send('小吉ですね！ちょっと優しくされるかも？')
+        
+        elif prob < 0.83:
+            await message.channel.send('吉の中で1番当たっても微妙に感じられる……つまり末吉なのです( ´･ω･`)')
+       
+        elif prob <= 1.0:
+            await message.channel.send('おめでとうございます！大吉ですよ！(๑>∀<๑)♥')   
 
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
