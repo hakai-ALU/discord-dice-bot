afbcf5b9c263b26eed77850defcf234dfe8a038e
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-08-03 20:55:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -66,7 +66,7 @@ async def on_message(message):
 
 @client.event
 async def on_member_join(member):
-    await client.get_channel(CHANNEL_ID4).send(message.author.id)     
+    await client.get_channel(CHANNEL_ID4).send(join.author.id)     
 
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
