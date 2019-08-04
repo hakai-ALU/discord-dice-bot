2e657b2ffc0df0a21aefbb01674fac35c118adf2
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-08-04 20:42:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,8 @@
 import discord 
 import os
-from discord.ext import tasks
-from datetime import datetime
-import random
-import re
 
 #トークン
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
-
-#チャンネルID
-CHANNEL_ID = 587658526013390859 
-CHANNEL_ID2 = 603203288883789831
-CHANNEL_ID3 = 604573398831726602
-CHANNEL_ID4 = 606100494179500054
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -20,66 +10,12 @@ client = discord.Client()
 #起動メッセージ
 @client.event
 async def on_ready():
-    print('Hello World,リマインドbotプログラム「project-remain」、起動しました')
-    channel = client.get_channel(CHANNEL_ID2)
-    await channel.send('BOT再起動しました。')   
+    print('Hello World,リマインドbotプログラム「project-youtube」、起動しました')
 
 @client.event
 async def on_message(message):
     """メッセージを処理"""
     if message.author.bot:  # ボットのメッセージをハネる
         return
-    
-#おみくじ
-    if message.content == "おみくじ":
-        # Embedを使ったメッセージ送信 と ランダムで要素を選択
-        embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢は！",
-                              color=0x2ECC69)
-        embed.set_thumbnail(url=message.author.avatar_url)
-        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '中吉', '小吉', '吉', '半吉', '末吉', '末小吉', '凶', '小凶', '半凶', '末凶', '大凶')), inline=False)
-        await client.get_channel(CHANNEL_ID3).send(embed=embed)
-
- #運勢
-    if message.content == '運勢':
-        prob = random.random()
-    
-        if prob < 0.3:
-            await client.get_channel(CHANNEL_ID3).send('凶です……外出を控えることをオススメします(  ･᷄ὢ･᷅  )')
-        
-        elif prob < 0.65:
-            await client.get_channel(CHANNEL_ID3).send('吉です！何かいい事があるかもですね！')
-        
-        elif prob < 0.71:
-            await client.get_channel(CHANNEL_ID3).send('末吉……どれくらい運がいいんでしょうね？•́ω•̀)?')
-        
-        elif prob < 0.76:
-            await client.get_channel(CHANNEL_ID3).send('半吉は吉の半分、つまり運がいいのです！')
-        
-        elif prob < 0.80:
-            await client.get_channel(CHANNEL_ID3).send('小吉ですね！ちょっと優しくされるかも？')
-        
-        elif prob < 0.83:
-            await client.get_channel(CHANNEL_ID3).send('吉の中で1番当たっても微妙に感じられる……つまり末吉なのです( ´･ω･`)')
-       
-        elif prob <= 1.0:
-            await client.get_channel(CHANNEL_ID3).send('おめでとうございます！大吉ですよ！(๑>∀<๑)♥')   
-
-@client.event
-async def on_member_join(member):
-    await client.get_channel(CHANNEL_ID4).send(join.author.id)     
-
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '09:00':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('マルキュマルマル！おはようございます！今日も一日頑張りましょう！')  
-    elif now == '23:00':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('フタサンマルマル！おやすみなさい！以降のメンションはお控え下さい。') 
-#ループ処理実行
-loop.start()
 
 client.run(TOKEN)
