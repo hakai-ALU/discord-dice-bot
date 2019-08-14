6d54a9ecbc6a4d086e9a4df14cd2bfe99616b9be
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-08-14 21:17:23 +0900

    Add files via upload

---
+++ discordDice.py
@@ -0,0 +1,18 @@
+import discord
+import nDnDICE
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Botを起動しました。')
+
+@client.event
+async def on_message(message):
+    msg = message.content
+    result = nDnDICE.nDn(msg)
+    if result is not None:
+        await client.send_message(message.channel, result)
+    
+#ここにbotのアクセストークンを入力
+client.run('***')
