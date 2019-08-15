e3730644a8f0d4a9a902e502c6f5326a795eecfc
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-08-15 20:01:46 +0900

    Update and rename discordbot2.py to discordbot.py

---
+++ discordbot.py
@@ -0,0 +1,29 @@
+import discord 
+import os
+
+#トークン
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+#チャンネルID
+CHANNEL_ID = 610388405926494211
+
+#発言した奴のID取得
+@client.event
+async def on_message(message):
+    if not message.author.bot: 
+        await client.get_channel(CHANNEL_ID).send(message.author.id)  
+
+#入ってきた奴のID取得
+@client.event
+async def on_member_join(member):
+    await client.get_channel(CHANNEL_ID).send(member.id)            
+
+#出て行った奴のID取得
+@client.event
+async def on_member_remove(member):
+    await client.get_channel(CHANNEL_ID).send(member.id)            
+    
+client.run(TOKEN)
