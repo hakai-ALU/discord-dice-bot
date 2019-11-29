0d40022b21762203f0ec4059649351d3a3d8aa5c
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-11-29 22:40:00 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,8 +8,6 @@ TOKEN = os.environ['DISCORD_BOT_TOKEN']
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-channel_bot_test = 'noa-global-chat'
-
 #起動メッセージ
 @client.event
 async def on_ready():
@@ -17,11 +15,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if channel.name = channel_bot_test
-        if '/discord.gg/' in message.content:
-            await asyncio.sleep(1)
-            await message.delete()
-
+    
     if 'Bumpを確認しました' in message.content:
         await asyncio.sleep(1)
         await message.channel.send('bumpを確認しました！2時間後お願いします！') 
