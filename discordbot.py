f32073610eff4182b617fa44cf7eaf83268666c4
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-11-29 18:28:30 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if '23456' in message.content:
+    if '表示順をアップしたよ' in message.content:
         await channel.send('bumpを確認しました！2時間後お願いします！') 
         await asyncio.sleep(7200)
         await channel.send('bumpチャンス！') 
