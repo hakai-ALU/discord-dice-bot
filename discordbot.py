cc30d334ef766a0ea1280fa1d5b2a03f60482951
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-11-29 18:22:14 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,6 @@
 import discord 
 import os
+import asyncio
 
 #トークン
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -10,35 +11,18 @@ client = discord.Client()
 #起動メッセージ
 @client.event
 async def on_ready():
-    client.global_list = []
     print("起動しました")
 
 @client.event
 async def on_message(message):
 
+    if '23456' in message.content:
+        await channel.send('bumpを確認しました！2時間後お願いします！') 
+        await asyncio.sleep(7200)
+        await channel.send('bumpチャンス！') 
+
     if message.author == message.guild.me:
         return
-
-    if message.webhook_id:
-        return
-
-    global_tmp = [w for w in await message.channel.webhooks() if w in client.global_list]
-
-    if message.content == "!noa-global":
-        if message.author.guild_permissions.administrator:
-            if global_tmp:
-                await message.channel.send("既に登録されています。")
-                return
-
-            new_w = await message.channel.create_webhook(name="global")
-            client.global_list.append(new_w)
-            await message.channel.send("グローバルチャットのチャンネルに登録しました。")
-            return
-
-    for webhook in client.global_list:
-        if message.channel != webhook.channel:
-            await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url)
-
 
 client.run(TOKEN)
 
