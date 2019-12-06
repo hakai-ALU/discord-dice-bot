e6be98dec8267307e67b2db0465ac7556711ea69
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-12-06 11:08:28 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,9 +23,38 @@ async def on_message(message):
         await asyncio.sleep(2*60*60)
         await message.channel.send('<@&650506130325372950> bumpチャンス！') 
 
-    if 'Bumpを確認しました' in message.content:
-        if 'Bumpを確認しました' in message.content:
-            
+    if 'joined; Invited by DISBOARD' in message.content:
+        if '(7 invites)' in message.content:
+            role1 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role1)
+        if '(14 invites)' in message.content:
+            role2 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role2)
+        if '(21 invites)' in message.content:
+            role3 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role3)
+        if '(28 invites)' in message.content:
+            role4 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role4)
+        if '(35 invites)' in message.content:
+            role5 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role5)
+        if '(42 invites)' in message.content:
+            role6 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role6)
+        if '(49 invites)' in message.content:
+            role7 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role7)
+        if '(56 invites)' in message.content:
+            role8 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role8)
+        if '(63 invites)' in message.content:
+            role9 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role9)
+        if '(70 invites)' in message.content:
+            role10 = discord.utils.get(message.guild.roles, name='class SAXONY')
+            await message.author.add_roles(role10)
+
 
 client.run(TOKEN)
 
