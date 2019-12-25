73720b0d03efb2e6b2c4f4d84338912d21680e5c
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-12-25 21:58:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -48,7 +48,27 @@ async def on_message(message):
             embed = discord.Embed(title="サーバーアイコン")
             embed.set_image(url=message.guild.icon_url)
             await message.channel.send(embed=embed)
-    
+        if not message.author.guild_permissions.administrator:
+            await message.channel.send('貴方は管理者権限がありません。 \n You do not have admin roles !!')
+
+    if message.content == 'ステータスE':
+        if message.author.id == master_owner_id:
+            embed = discord.Embed(title="この鯖のステータス",description="Embed式")
+            embed.add_field(name="サーバー名",value=f'{message.guild.name}',inline=False)
+            embed.add_field(name="現オーナー名",value=f'{message.guild.owner}',inline=False)
+            guild = message.guild
+            member_count = sum(1 for member in guild.members if not member.bot) 
+            bot_count = sum(1 for member in guild.members if member.bot) 
+            all_count = (member_count) + (bot_count)
+            embed.add_field(name="総人数",value=f'{all_count}',inline=False)
+            embed.add_field(name="ユーザ数",value=f'{member_count}',inline=False)
+            embed.add_field(name="BOT数",value=f'{bot_count}',inline=False)
+            embed.add_field(name="総チャンネル数",value=f'{len(message.guild.channels)}個',inline=False)
+            embed.add_field(name="テキストチャンネル数",value=f'{len(message.guild.text_channels)}個',inline=False)
+            embed.add_field(name="ボイスチャンネル数",value=f'{len(message.guild.voice_channels)}個',inline=False)
+            embed.set_thumbnail(url=message.guild.icon_url)
+            await message.channel.send(embed=embed)
+
         if not message.author.guild_permissions.administrator:
             await message.channel.send('貴方は管理者権限がありません。 \n You do not have admin roles !!')
               
