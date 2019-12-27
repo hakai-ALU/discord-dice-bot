0928d4574858e8c1816c312d95d59f245dcb2f93
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-12-27 20:33:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 #トークン
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
+CHANNEL_ID = 648435960077615127
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -13,7 +14,19 @@ client = discord.Client()
 #起動メッセージ
 @client.event
 async def on_ready():
-    print("起動しました")
+    print(client.user.name)  # ボットの名前
+    print(client.user.id)  # ボットのID
+    print(discord.__version__)  # discord.pyのバージョン
+    print('----------------')
+    print('Hello World,リマインドbotプログラム「project-RRN」、起動しました')
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send(f'名前:{client.user.name}')  # ボットの名前
+    await channel.send(f'ID:{client.user.id}')  # ボットのID
+    await channel.send(f'Discord ver:{discord.__version__}')  # discord.pyのバージョン
+    await channel.send('----------------')
+    await channel.send('状態：BOT再起動しました。')   
+    await client.change_presence(status=discord.Status.idle,activity=discord.Game(name='ギルド専属ナビ'))
+    
 
 @client.event
 async def on_message(message):
