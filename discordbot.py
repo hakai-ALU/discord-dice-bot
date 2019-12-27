939f72de3e10fc0e374c99556882aec3c5ec2643
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2019-12-27 20:47:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@ import os
 import asyncio
 from discord.ext import tasks
 from datetime import datetime
+import re
 
 #トークン
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -10,6 +11,26 @@ CHANNEL_ID = 648435960077615127
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
+
+async def message_count(channel):
+    """チャンネル名にあるメッセージ数を1増やす"""
+    matched = re.match(r"(.+)（(\d+)）",channel.name)
+
+    if matched:
+        #もし、メッセージのカウントが既に行われていたら
+        base_name = matched.groups()[0] # 元のチャンネル名
+        count = int(matched.groups()[1]) # メッセージ数
+        await channel.edit(name=f"{base_name}（{count+1}）")
+        return
+
+    count = 0
+    async for message in channel.history(limit=None):
+        if not message.author.bot:
+            # 人間が投稿していたらカウントを1増やす
+            count += 1 
+
+    await channel.edit(name=f"{channel.name}（{count}）")
+
 
 #起動メッセージ
 @client.event
@@ -93,6 +114,17 @@ async def on_message(message):
         date = datetime.now()
         await message.channel.send(f'今は{date.hour}時{date.minute}分{date.second}秒だよ！')
 
+    if message.channel.category_id != 652032561815486486:
+        # 指定したIDのカテゴリのチャンネルに対してのみ処理する
+        return
+
+    if message.channel.position != 0:
+        # もし一番上のチャンネルでなければ
+        if not message.content.startswith("[sage]"):
+        # [sage]で始まっていなければ
+            await message.channel.edit(position=0)
+
+    await message_count(message.channel)
               
 client.run(TOKEN)
 
