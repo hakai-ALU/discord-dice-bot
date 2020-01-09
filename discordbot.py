852db5df82806b7630b49901fdea1828e1a1c52c
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-01-10 02:14:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,26 +13,6 @@ CHANNEL_ID3 = 664098210264121374
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-async def message_count(channel):
-    """チャンネル名にあるメッセージ数を1増やす"""
-    matched = re.match(r"(.+)（(\d+)）",channel(CHANNEL_ID3).name)
-
-    if matched:
-        #もし、メッセージのカウントが既に行われていたら
-        base_name = matched.groups()[0] # 元のチャンネル名
-        count = int(matched.groups()[1]) # メッセージ数
-        await message.channel(CHANNEL_ID3).edit(name=f"{base_name}（{count+1}）")
-        return
-
-    count = 0
-    async for message in channel.history(limit=None):
-        if not message.author.bot:
-            # 人間が投稿していたらカウントを1増やす
-            count += 1 
-
-    await message.channel(CHANNEL_ID3).edit(name=f"{channel.name}（{count}）")
-
-
 #起動メッセージ
 @client.event
 async def on_ready():
