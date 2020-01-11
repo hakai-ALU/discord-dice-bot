caefb80ea7fba2554a85554c2ce72e6a66f39e6d
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-01-12 00:37:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ TOKEN = os.environ['DISCORD_BOT_TOKEN']
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-CHANNEL_ID = 
+CHANNEL_ID = 665579602504318978
 
 #起動メッセージ
 @client.event
@@ -16,16 +16,15 @@ async def on_ready():
     print(client.user.id)  # ボットのID
     print(discord.__version__)  # discord.pyのバージョン
     print('----------------')
-    print('Hello World,Discord-Test-Program 通称「DTP」、起動しました')
+    print('Hello World,『Discord-Test-Program』通称「DTP」、起動しました')
     channel = client.get_channel(CHANNEL_ID)
     await channel.purge()
     await channel.send(f'名前:{client.user.name}')  # ボットの名前
     await channel.send(f'ID:{client.user.id}')  # ボットのID
     await channel.send(f'Discord ver:{discord.__version__}')  # discord.pyのバージョン
     await channel.send('----------------')
-    await channel.send('状態：BOT再起動しました。')   
-    await client.change_presence(status=discord.Status.idle,activity=discord.Game(name='ギルド専属ナビ'))
-    
+    await channel.send('状態：DTP起動しました。')   
+    await client.change_presence(status=discord.Status.idle,activity=discord.Game(name='DTP起動'))    
 
 @client.event
 async def on_message(message):
