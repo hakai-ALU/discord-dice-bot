155b889d93a1728e8aeec86ed658ec7ef76ed052
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-01-12 00:34:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,13 @@
 import discord 
 import os
-import asyncio
-from discord.ext import tasks
-from datetime import datetime
-import re
-import random
 
 #トークン
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 648435960077615127
-great_owner_id = 459936557432963103
-CHANNEL_ID3 = 664098210264121374
+
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
+
+CHANNEL_ID = 
 
 #起動メッセージ
 @client.event
@@ -21,7 +16,7 @@ async def on_ready():
     print(client.user.id)  # ボットのID
     print(discord.__version__)  # discord.pyのバージョン
     print('----------------')
-    print('Hello World,リマインドbotプログラム「project-RRN」、起動しました')
+    print('Hello World,Discord-Test-Program 通称「DTP」、起動しました')
     channel = client.get_channel(CHANNEL_ID)
     await channel.purge()
     await channel.send(f'名前:{client.user.name}')  # ボットの名前
@@ -35,106 +30,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     
-    if 'Bumpを確認しました' in message.content:
-        await message.channel.send('bumpを確認しました！2時間後お願いします！') 
-        await asyncio.sleep(2*60*60)
-        await message.channel.send('<@&650506130325372950> bumpチャンス！') 
-
     if message.author.bot:  # ボットを弾く。
         return 
-
-    if message.content == 'ステータス':
-        if message.author.guild_permissions.administrator:
-            await message.channel.send(f'サーバー名：{message.guild.name}')
-            await asyncio.sleep(0.1)
-            await message.channel.send(f'現オーナー名：{message.guild.owner}')
-            await asyncio.sleep(0.1)
-            guild = message.guild
-            member_count = sum(1 for member in guild.members if not member.bot) 
-            bot_count = sum(1 for member in guild.members if member.bot) 
-            all_count = (member_count) + (bot_count)
-            await message.channel.send(f'総人数：{all_count}人')
-            await asyncio.sleep(0.1)
-            await message.channel.send(f'ユーザ数：{member_count}')
-            await asyncio.sleep(0.1)
-            await message.channel.send(f'BOT数：{bot_count}')
-            await asyncio.sleep(0.1) 
-            await message.channel.send(f'総チャンネル数：{len(message.guild.channels)}個')
-            await asyncio.sleep(0.1)
-            await message.channel.send(f'テキストチャンネル数：{len(message.guild.text_channels)}個')
-            await asyncio.sleep(0.1)
-            await message.channel.send(f'ボイスチャンネル数：{len(message.guild.voice_channels)}個')
-            await asyncio.sleep(0.1)
-            embed = discord.Embed(title="サーバーアイコン")
-            embed.set_image(url=message.guild.icon_url)
-            await message.channel.send(embed=embed)
-        if not message.author.guild_permissions.administrator:
-            await message.channel.send('貴方は管理者権限がありません。 \n You do not have admin roles !!')
-
-    if message.content == 'ステータスE':
-        if message.author.guild_permissions.administrator:
-            embed = discord.Embed(title="この鯖のステータス",description="Embed式")
-            embed.add_field(name="サーバー名",value=f'{message.guild.name}',inline=False)
-            embed.add_field(name="現オーナー名",value=f'{message.guild.owner}',inline=False)
-            guild = message.guild
-            member_count = sum(1 for member in guild.members if not member.bot) 
-            bot_count = sum(1 for member in guild.members if member.bot) 
-            all_count = (member_count) + (bot_count)
-            embed.add_field(name="総人数",value=f'{all_count}',inline=False)
-            embed.add_field(name="ユーザ数",value=f'{member_count}',inline=False)
-            embed.add_field(name="BOT数",value=f'{bot_count}',inline=False)
-            embed.add_field(name="総チャンネル数",value=f'{len(message.guild.channels)}個',inline=False)
-            embed.add_field(name="テキストチャンネル数",value=f'{len(message.guild.text_channels)}個',inline=False)
-            embed.add_field(name="ボイスチャンネル数",value=f'{len(message.guild.voice_channels)}個',inline=False)
-            embed.set_thumbnail(url=message.guild.icon_url)
-            await message.channel.send(embed=embed)
-
-        if not message.author.guild_permissions.administrator:
-            await message.channel.send('貴方は管理者権限がありません。 \n You do not have admin roles !!')
-
-        #年月日
-    if message.content.startswith('何日？') or message.content == '何日?':
-        date = datetime.now()
-        await message.channel.send(f'今日は{date.year}年{date.month}月{date.day}日です！')    
-    if message.content.startswith('何時？') or message.content == '何時?':
-        date = datetime.now()
-        await message.channel.send(f'今は{date.hour}時{date.minute}分{date.second}秒だよ！')
-
-    if message.content == 'nrestart': 
-        if message.author.id == great_owner_id:
-            await message.channel.send('再起動します')
-            await asyncio.sleep(0.5)
-            await client.logout()  
-            os.execv(sys.executable,[sys.executable, os.path.join(sys.path[0], __file__)] + sys.argv[1:])  
-        if not message.author.id == great_owner_id:
-            await message.channel.send('貴方にこのコマンドの使用権限はありません')   
-
-    if message.content == 'nclear': 
-        if message.author.id == great_owner_id:
-            await message.channel.purge()  
-        if not message.author.id == great_owner_id:
-            await message.channel.send('貴方にこのコマンドの使用権限はありません')   
-
-    if not message.author.id == 664880378481213473:
-        prob = random.random()
-    
-        if prob < 0.35:
-            await message.add_reaction('💝')
-
-    if message.channel.category_id != 662588978029002763:
-        # 指定したIDのカテゴリのチャンネルに対してのみ処理する
-        return
-
-    if message.channel.position != 0:
-        # もし一番上のチャンネルでなければ
-        if not message.content.startswith("[sage]"):
-        # [sage]で始まっていなければ
-            await client.get_channel(CHANNEL_ID3).edit(position=0)
-
-    await message_count(message.channel)       
-
               
 client.run(TOKEN)
-
-#ノア
-#グローバルチャット
