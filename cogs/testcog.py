c054b66d9821755514cb9acf6b5eb8036bbab4b2
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-01-23 06:13:17 +0900

    Update testcog.py

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -16,6 +16,33 @@ class TestCog(commands.Cog):
     async def what(self, ctx, what):
         await ctx.send(f'{what}とはなんですか？')
 
+    # メインとなるroleコマンド
+    @commands.group()
+    async def role(self, ctx):
+        # サブコマンドが指定されていない場合、メッセージを送信する。
+        if ctx.invoked_subcommand is None:
+            await ctx.send('このコマンドにはサブコマンドが必要です。')
+
+    # roleコマンドのサブコマンド
+    # 指定したユーザーに指定した役職を付与する。
+    @role.command()
+    async def add(self, ctx, member: discord.Member, role: discord.Role):
+        await member.add_roles(role)
+
+    # roleコマンドのサブコマンド
+    # 指定したユーザーから指定した役職を剥奪する。
+    @role.command()
+    async def remove(self, ctx, member: discord.Member, role: discord.Role):
+        await member.remove_roles(role)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.content == 'こんにちは':
+            await message.channel.send('こんにちは')
+
 # Bot本体側からコグを読み込む際に呼び出される関数。
 def setup(bot):
     bot.add_cog(TestCog(bot)) # TestCogにBotを渡してインスタンス化し、Botにコグとして登録する。
