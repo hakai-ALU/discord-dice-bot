399ce40e57869fcb0cfac7922200b5ed5036b221
ATTILA0516 <46132263+ATTILA0516@users.noreply.github.com>
2020-01-23 11:52:43 +0900

    Update testcog.py

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -30,7 +30,7 @@ class TestCog(commands.Cog):
 
     # roleコマンドのサブコマンド
     # 指定したユーザーに指定した役職を付与する。
-    @role.command()
+    @role.command(aiiases=['cr'])
     async def create(self, ctx):
         guild = ctx.guild
         set1 = random.choice(('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'))
@@ -43,13 +43,13 @@ class TestCog(commands.Cog):
         
     # roleコマンドのサブコマンド
     # 指定したユーザーに指定した役職を付与する。
-    @role.command()
+    @role.command(aiiases=['ad'])
     async def add(self, ctx, member: discord.Member, role: discord.Role):
         await member.add_roles(role)
 
     # roleコマンドのサブコマンド
     # 指定したユーザーから指定した役職を剥奪する。
-    @role.command()
+    @role.command(aiiases=['rm'])
     async def remove(self, ctx, member: discord.Member, role: discord.Role):
         await member.remove_roles(role)
 
