c3616704ae8319965b765fb3471dee4f72f91437
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-03-26 22:04:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,6 @@ import os
 import discord
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 665579602504318978
 
 # 読み込むコグの名前を格納しておく。
 INITIAL_EXTENSIONS = [
@@ -34,16 +33,8 @@ class MyBot(commands.Bot):
         print(discord.__version__)  # discord.pyのバージョン
         print('----------------')
         print('Hello World,リマインドbotプログラム「project-RRN」、起動しました')
-        channel = self.get_channel(CHANNEL_ID)
-        await channel.purge()
-        await channel.send(f'名前:{self.user.name}')  # ボットの名前
-        await channel.send(f'ID:{self.user.id}')  # ボットのID
-        await channel.send(f'Discord ver:{discord.__version__}')  # discord.pyのバージョン
-        await channel.send('----------------')
-        await channel.send('状態：BOT再起動しました。')   
-        await self.change_presence(status=discord.Status.idle,activity=discord.Game(name='Discord Test Program'))
         
 # MyBotのインスタンス化及び起動処理。
 if __name__ == '__main__':
-    bot = MyBot(command_prefix='rn!') # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
+    bot = MyBot(command_prefix='rt') # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
     bot.run(TOKEN) # Botのトークン
