6c7531a3d755dad2dec74039d9a6f01d9bb46f24
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-02 19:58:56 +0900

    Update testcog.py

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -95,7 +95,7 @@ class TestCog(commands.Cog):
     @commands.has_permissions(manage_guild=True)
     async def ban(self, ctx, member: int=None, reason =None):
         if member != None:
-            member = discord.Object(user_id)
+            member = discord.Object(member)
         banlog = self.bot.get_channel(694044656501129317)
         if member == None or member == ctx.message.author:
             await ctx.channel.send("BAN対象が正しくありません")
