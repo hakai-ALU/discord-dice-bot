7004d18188e477991375b14c8c6bdc79f3009925
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-04 18:38:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,10 @@ class MyBot(commands.Bot):
         # スーパークラスのコンストラクタに値を渡して実行。
         super().__init__(command_prefix,help_command)
 
+        self.commands_heading = "コマンド:"
+        self.no_category = "その他"
+        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
+
         # INITIAL_COGSに格納されている名前から、コグを読み込む。
         # エラーが発生した場合は、エラー内容を表示。
         for cog in INITIAL_EXTENSIONS:
@@ -24,6 +28,10 @@ class MyBot(commands.Bot):
                 self.load_extension(cog)
             except Exception:
                 traceback.print_exc()
+
+    def get_ending_note(self):
+        return (f"各コマンドの説明: {prefix}help <コマンド名>\n"
+                f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
 
     # Botの準備完了時に呼び出されるイベント
     async def on_ready(self):
@@ -38,20 +46,9 @@ class MyBot(commands.Bot):
         await channel.send(discord.__version__)  # discord.pyのバージョン
         await channel.send('----------------')
         await channel.send('Hello World,五皇帝管理プログラム「project-RTA」、起動しました')
-        await self.change_presence(status=discord.Status.idle,activity=discord.Game(name=f'五皇管理システム|Ping:{self.ws.latency * 1000:.0f}ms'))
- 
-class JapaneseHelpCommand(commands.DefaultHelpCommand):
-    def __init__(self):
-        super().__init__()
-        self.commands_heading = "コマンド:"
-        self.no_category = "その他"
-        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
-
-    def get_ending_note(self):
-        return (f"各コマンドの説明: {prefix}help <コマンド名>\n"
-                f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
-
+        await self.change_presence(status=discord.Status.idle,activity=discord.Game(name=f'五皇管理システム|Ping:{self.ws.latency * 1000:.0f}ms'))  
+    
 #MyBotのインスタンス化及び起動処理。
 if __name__ == '__main__':
-    bot = MyBot(command_prefix='rt',help_command=commands.JapaneseHelpCommand()) # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
+    bot = MyBot(command_prefix='rt',help_command=commands.MyBot()) # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
     bot.run(TOKEN) # Botのトークン
