e61e0e5bcc571e07f8f5bd3903bc0aac57ecfa3a
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-22 09:45:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -54,5 +54,5 @@ class JapaneseHelpCommand(commands.DefaultHelpCommand):
 
 #MyBotのインスタンス化及び起動処理。
 if __name__ == '__main__':
-    bot = MyBot(command_prefix=prefix,help_command=JapaneseHelpCommand()) # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
+    bot = MyBot(command_prefix='rt',help_command=JapaneseHelpCommand()) # command_prefixはコマンドの最初の文字として使うもの。 e.g. !ping
     bot.run(TOKEN) # Botのトークン
