2dacf2b71aab90f44fb1c8ac56e871797e75d3b4
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-23 18:19:05 +0900

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -1,1 +1,73 @@
+from discord.ext import commands # Bot Commands Frameworkのインポート
+import discord
+import asyncio
+import random
+import datetime
 
+great_owner_id = 459936557432963103
+
+# コグとして用いるクラスを定義。
+class info(commands.Cog):
+    # infoクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['sinfo'])
+    async def serverinfo(self, ctx, server_id: int=None):
+        """鯖について"""
+        if server_id == None:
+            embed = discord.Embed(title="鯖ステータス",description=f"Ping:`{self.bot.ws.latency * 1000:.0f}ms`")
+            embed.add_field(name="サーバー名",value=f'`{ctx.guild.name}`')
+            embed.add_field(name="現オーナー名",value=f'`{ctx.guild.owner}`')
+            guild = ctx.guild
+            member_count = sum(1 for member in guild.members if not member.bot) 
+            bot_count = sum(1 for member in guild.members if member.bot) 
+            all_count = (member_count) + (bot_count)
+            embed.add_field(name="総人数",value=f'`{all_count}`',inline=False)
+            embed.add_field(name="ユーザ数",value=f'`{member_count}`',inline=False)
+            embed.add_field(name="BOT数",value=f'`{bot_count}`',inline=False)
+            embed.add_field(name="テキストチャンネル数",value=f'`{len(ctx.guild.text_channels)}`',inline=False)
+            embed.add_field(name="ボイスチャンネル数",value=f'`{len(ctx.guild.voice_channels)}`',inline=False)
+            embed.set_thumbnail(url=ctx.guild.icon_url)
+            await ctx.channel.send(embed=embed)
+            return
+        server = self.bot.get_guild(server_id)
+        embed = discord.Embed(title="鯖ステータス",description=f"Ping:`{self.bot.ws.latency * 1000:.0f}ms`")
+        embed.add_field(name="サーバー名",value=f'`{server.name}`')
+        embed.add_field(name="現オーナー名",value=f'`{server.owner}`')
+        guild = server
+        member_count = sum(1 for member in guild.members if not member.bot) 
+        bot_count = sum(1 for member in guild.members if member.bot) 
+        all_count = (member_count) + (bot_count)
+        embed.add_field(name="総人数",value=f'`{all_count}`',inline=False)
+        embed.add_field(name="ユーザ数",value=f'`{member_count}`',inline=False)
+        embed.add_field(name="BOT数",value=f'`{bot_count}`',inline=False)
+        embed.add_field(name="テキストチャンネル数",value=f'`{len(server.text_channels)}`',inline=False)
+        embed.add_field(name="ボイスチャンネル数",value=f'`{len(server.voice_channels)}`',inline=False)
+        embed.set_thumbnail(url=server.icon_url)
+        await ctx.channel.send(embed=embed)
+
+    @commands.command(aliases=['b'])
+    async def bot(self, ctx):
+        """Botについて"""
+        embed = discord.Embed(title=f"{self.bot.user}", description="このBotの情報です")
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+        embed.add_field(name="SERVERの数", value=f'`{len(self.bot.guilds)}`',inline=False)
+        embed.add_field(name="USERの数", value=f'`{len(set(self.bot.get_all_members()))}`',inline=False)
+        embed.add_field(name="言語", value='`discord.py`\n`discord.js`',inline=False)
+        embed.add_field(name="Ping値", value=f'`{self.bot.ws.latency * 1000:.0f}ms`',inline=False)
+        embed.add_field(name="各種リンク", value="[このBOTの公式開発鯖](<https://discord.gg/ENxnsJM>)", inline=False)
+        await ctx.channel.send(embed=embed)
+            
+    @commands.command(aliases=['t'])
+    async def test(self, ctx, user_id: int):
+        """ユーザーについて(テスト)"""
+        memberss = self.bot.get_user(user_id)
+        await ctx.send(f'{memberss}')
+        await ctx.send(f'{memberss.name}')
+        await ctx.send(f'{memberss.id}')
+        await ctx.send(f'{memberss.created_at}')
+
+# Bot本体側からコグを読み込む際に呼び出される関数。
+def setup(bot):
+    bot.add_cog(info(bot)) # mainにBotを渡してインスタンス化し、Botにコグとして登録する。        
