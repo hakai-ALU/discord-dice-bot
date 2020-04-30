3e957754f126f9b2465cd7158759aa857e7c8fb7
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-04-30 22:59:09 +0900

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -1,1 +1,13 @@
+from discord.ext import commands # Bot Commands Frameworkのインポート
+import discord
+import asyncio
 
+# コグとして用いるクラスを定義。
+class test(commands.Cog):
+    # testクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
+    def __init__(self, bot):
+        self.bot = bot
+
+# Bot本体側からコグを読み込む際に呼び出される関数。
+def setup(bot):
+    bot.add_cog(test(bot)) # mainにBotを渡してインスタンス化し、Botにコグとして登録する。
