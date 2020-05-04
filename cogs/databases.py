2fe8f3fa58b0037cae03bd38470e6eb9800e2fd4
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-04 21:46:05 +0900

    Update databases.py

--- cogs/databases.py
+++ cogs/databases.py
@@ -7,5 +7,16 @@ class Example(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def set(self, ctx, what1, what2):
+        conn = rdb.connect()
+        conn.set(what1, what2)
+
+    @commands.command()
+    async def get(self, ctx, what1):
+        conn = rdb.connect()
+        p = conn.get(what1)
+        await ctx.send(p)
+
 def setup(bot):
     bot.add_cog(Example(bot))
