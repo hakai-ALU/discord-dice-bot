836da624b7e2c3682c941edfda319c1f5ce12125
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-04 21:59:46 +0900

    Update rdb.py

--- rdb.py
+++ rdb.py
@@ -1,7 +1,7 @@
 import redis
 import os
 
-def connect():
+def con():
     return redis.from_url(
         url=os.environ.get('REDIS_URL'), # 環境変数にあるURLを渡す
         decode_responses=True, # 日本語の文字化け対策のため必須
