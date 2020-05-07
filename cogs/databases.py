b19f33f4d1e232e3972fce6f34d194d7254a15c9
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-07 22:25:22 +0900

    Update databases.py

--- cogs/databases.py
+++ cogs/databases.py
@@ -11,11 +11,10 @@ class Example(commands.Cog):
     async def bet(self, ctx, what:int):
         if ctx.author.id == 459936557432963103:
             conn = r.connect()
-            bt = conn.sadd("bet", what)
+            M = conn.get("M")
+            bt = conn.set(f"bet{M}", what)
             await ctx.send(bt)
-            bts = conn.smembers("bet")
-            await ctx.send("`" + str(bts) + "`")
-
+            
     @commands.command()
     async def delbet(self, ctx):
         conn = r.connect()
@@ -25,48 +24,24 @@ class Example(commands.Cog):
     @commands.command()
     async def set(self, ctx, what1, what2):
         conn = r.connect()
-        betatester = conn.smembers("bet")
-        if ctx.author.id in betatester:
-            await ctx.send("True")
-        else:
-            await ctx.send("貴方はベータテスターではありません。")
-            return
         q = conn.set(what1, what2)
         await ctx.send(q)
 
     @commands.command()
     async def get(self, ctx, what1):
         conn = r.connect()
-        betatester = conn.smembers("bet")
-        if ctx.author.id in betatester:
-            await ctx.send("True")
-        else:
-            await ctx.send("貴方はベータテスターではありません。")
-            return
         p = conn.get(what1)
         await ctx.send(p)
 
     @commands.command()
     async def sadd(self, ctx, what):
         conn = r.connect()
-        betatester = conn.smembers("bet")
-        if ctx.author.id in betatester:
-            await ctx.send("True")
-        else:
-            await ctx.send("貴方はベータテスターではありません。")
-            return
         q = conn.sadd("what2", what)
         await ctx.send(q)
 
     @commands.command()
     async def smembers(self, ctx):
         conn = r.connect()
-        betatester = conn.smembers("bet")
-        if ctx.author.id in betatester:
-            await ctx.send("True")
-        else:
-            await ctx.send("貴方はベータテスターではありません。")
-            return
         p = conn.smembers("what2")
         for w in p:
             await ctx.send(w)
@@ -74,13 +49,6 @@ class Example(commands.Cog):
     @commands.command()
     async def key(self, ctx):
         conn = r.connect()
-        betatester = conn.smembers("bet")
-        await ctx.send(betatester.decode())
-        if ctx.author.id in betatester:
-            await ctx.send("True")
-        else:
-            await ctx.send("貴方はベータテスターではありません。")
-            return
         ak = conn.keys()
         for k in ak:
             await ctx.send(k)
@@ -88,24 +56,12 @@ class Example(commands.Cog):
     @commands.command()
     async def delete(self, ctx, what):
         conn = r.connect()
-        betatester = conn.smembers("bet")
-        if ctx.author.id in betatester:
-            await ctx.send("True")
-        else:
-            await ctx.send("貴方はベータテスターではありません。")
-            return
         d = conn.delete(what)
         await ctx.send(d)
 
     @commands.command()
     async def alldelete(self, ctx):
         conn = r.connect()
-        betatester = conn.smembers("bet")
-        if ctx.author.id in betatester:
-            await ctx.send("True")
-        else:
-            await ctx.send("貴方はベータテスターではありません。")
-            return
         ad = conn.flushdb()
         await ctx.send(ad)
 
