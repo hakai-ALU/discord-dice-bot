682106447a779beb430d4a84998f36fc4338bca7
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-10 16:51:47 +0900

    Update scythe_member.py

--- cogs/scythe_member.py
+++ cogs/scythe_member.py
@@ -8,8 +8,8 @@ class scythe(commands.Cog):
         self.bot = bot
         self.name_bea = 0
 
-    @commands.command(name='登録')
-    async def touroku(self, ctx):
+    @commands.command(name="登録")
+    async def sighin(self, ctx):
         conn = r.connect()
         k = conn.keys()
         cai = ctx.author.id
@@ -24,56 +24,6 @@ class scythe(commands.Cog):
                 await ctx.send("登録に失敗しました。\nやり直して下さい。")
         else:
             await ctx.send("既に登録済みです。")
-
-    @commands.command()
-    async def delbet(self, ctx):
-        conn = r.connect()
-        ad = conn.flushdb()
-        await ctx.send(ad)  
-     
-    @commands.command()
-    async def set(self, ctx, what1, what2):
-        conn = r.connect()
-        q = conn.set(what1, what2)
-        await ctx.send(q)
-
-    @commands.command()
-    async def get(self, ctx, what1):
-        conn = r.connect()
-        p = conn.get(what1)
-        await ctx.send(p)
-
-    @commands.command()
-    async def sadd(self, ctx, what):
-        conn = r.connect()
-        q = conn.sadd("what2", what)
-        await ctx.send(q)
-
-    @commands.command()
-    async def smembers(self, ctx):
-        conn = r.connect()
-        p = conn.smembers("what2")
-        for w in p:
-            await ctx.send(w)
-
-    @commands.command()
-    async def key(self, ctx):
-        conn = r.connect()
-        ak = conn.keys()
-        for k in ak:
-            await ctx.send(k)
-
-    @commands.command()
-    async def delete(self, ctx, what):
-        conn = r.connect()
-        d = conn.delete(what)
-        await ctx.send(d)
-
-    @commands.command()
-    async def alldelete(self, ctx):
-        conn = r.connect()
-        ad = conn.flushdb()
-        await ctx.send(ad)
 
 def setup(bot):
     bot.add_cog(scythe(bot))
