4d37c0e413612b4b365fd762c72cf3ab0f2b6b81
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-10 22:25:38 +0900

    Update databases.py

--- cogs/databases.py
+++ cogs/databases.py
@@ -37,7 +37,7 @@ class Example(commands.Cog):
         if ctx.author.id != self.ahomin:
             return await ctx.send("貴方は使えません")
         conn = r.connect()
-        p = conn.smembers(what)
+        p = conn.smembers(f'{what}')
         for w in p:
             await ctx.send(w)
 
