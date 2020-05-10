1627f45e583c84c926268037515ec76dd0bd3dce
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-10 22:04:50 +0900

    Update databases.py

--- cogs/databases.py
+++ cogs/databases.py
@@ -6,63 +6,62 @@ class Example(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.ahomin = 459936557432963103
 
     @commands.command()
-    async def bet(self, ctx, what:int):
-        if ctx.author.id == 459936557432963103:
-            conn = r.connect()
-            u = conn.get("M")
-            bt = conn.set(f"bet{u}", what)
-            await ctx.send(bt)
-            v = int(u) + 1
-            bv = conn.set("M", v)
-            
-    @commands.command()
-    async def delbet(self, ctx):
-        conn = r.connect()
-        ad = conn.flushdb()
-        await ctx.send(ad)  
-     
-    @commands.command()
     async def set(self, ctx, what1, what2):
+        if ctx.author.id != self.ahomin:
+            return await ctx.send("貴方は使えません")
         conn = r.connect()
         q = conn.set(what1, what2)
         await ctx.send(q)
 
     @commands.command()
     async def get(self, ctx, what1):
+        if ctx.author.id != self.ahomin:
+            return await ctx.send("貴方は使えません")
         conn = r.connect()
         p = conn.get(what1)
         await ctx.send(p)
 
     @commands.command()
-    async def sadd(self, ctx, what):
+    async def sadd(self, ctx, what1, what2):
+        if ctx.author.id != self.ahomin:
+            return await ctx.send("貴方は使えません")
         conn = r.connect()
-        q = conn.sadd("what2", what)
+        q = conn.sadd(what1, what2)
         await ctx.send(q)
 
     @commands.command()
-    async def smembers(self, ctx):
+    async def smembers(self, ctx, whst):
+        if ctx.author.id != self.ahomin:
+            return await ctx.send("貴方は使えません")
         conn = r.connect()
-        p = conn.smembers("what2")
+        p = conn.smembers(what)
         for w in p:
             await ctx.send(w)
 
     @commands.command()
     async def key(self, ctx):
+        if ctx.author.id != self.ahomin:
+            return await ctx.send("貴方は使えません")
         conn = r.connect()
         ak = conn.keys()
         for k in ak:
-            await ctx.send(k)
+            await ctx.send(f"`{k}`")
 
     @commands.command()
     async def delete(self, ctx, what):
+        if ctx.author.id != self.ahomin:
+            return await ctx.send("貴方は使えません")
         conn = r.connect()
         d = conn.delete(what)
         await ctx.send(d)
 
     @commands.command()
     async def alldelete(self, ctx):
+        if ctx.author.id != self.ahomin:
+            return await ctx.send("貴方は使えません")
         conn = r.connect()
         ad = conn.flushdb()
         await ctx.send(ad)
