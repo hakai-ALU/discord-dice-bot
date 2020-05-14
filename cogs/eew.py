60b82fa710f47d07141fbe0d8ae8ee5e72fa1cc8
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-14 22:52:06 +0900

    Update eew.py

--- cogs/eew.py
+++ cogs/eew.py
@@ -17,6 +17,15 @@ class eew(commands.Cog):
         """Test1(開発者用)"""
         if ctx.author.id != great_owner_id:
             return
+        r = requests.get('http://svir.jp/eew/data.json')
+        eew = r.json()
+        embed=discord.Embed(title="**地震情報**", description=eew['Head']['Title'])
+        embed.add_field(name="発表時刻", value=eew['Body']['Earthquake']['OriginTime'], inline=False)
+        embed.add_field(name="震源地", value=eew['Body']['Earthquake']['Hypocenter']['Name'], inline=False)
+        embed.add_field(name="マグニチュード", value=eew['Body']['Earthquake']['Magnitude'], inline=False) 
+        embed.add_field(name="深さ", value=eew['Body']['Earthquake']['Hypocenter']['Depth'] + "km" , inline=False)
+        embed.add_field(name="予想震度[震源地付近の推定です]", value=eew['Body']['Intensity']['TextInt'], inline=False)
+        await ctx.send(embed=embed)
 
 # Bot本体側からコグを読み込む際に呼び出される関数。
 def setup(bot):
