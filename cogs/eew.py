93f097a3df152c76b670338934530ebe9c90d419
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-16 14:58:23 +0900

    Update eew.py

--- cogs/eew.py
+++ cogs/eew.py
@@ -31,7 +31,7 @@ class eew(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def eewop(self, ctx):
+    async def eewop1(self, ctx):
         """地震情報(確認用)"""
         resp = urllib.request.urlopen('http://svir.jp/eew/data.json')
         eew = json.loads(resp.read().decode('utf-8'))
@@ -45,7 +45,13 @@ class eew(commands.Cog):
         embed.add_field(name="**電文の配信数、地震識別IDごとに増えていく**", value=eew['Head']['Serial'], inline=False)
         embed.add_field(name="**電文バージョン**", value=eew['Head']['Version'], inline=False)
         await ctx.send(embed=embed)
-        
+ 
+    @commands.command()
+    async def eewop2(self, ctx):
+        """地震情報(確認用)"""
+        resp = urllib.request.urlopen('http://svir.jp/eew/data.json')
+        eew = json.loads(resp.read().decode('utf-8'))
+        #body       
         embed=discord.Embed(title="**地震発生時刻**", description=eew['Body']['Earthquake']['OriginTime'])
         embed.add_field(name="**震央地名**", value=eew['Body']['Earthquake']['Hypocenter']['Name'], inline=False) 
         embed.add_field(name="**震央地名コード**", value=eew['Body']['Earthquake']['Hypocenter']['Code'], inline=False)
@@ -54,14 +60,24 @@ class eew(commands.Cog):
         embed.add_field(name="**震源の深さ**", value=eew['Body']['Earthquake']['Hypocenter']['Depth'], inline=False)
         embed.add_field(name="**震源の陸海識別**", value=eew['Body']['Earthquake']['Hypocenter']['LandOrSea'], inline=False)
         await ctx.send(embed=embed)
-  
+ 
+    @commands.command()
+    async def eewop3(self, ctx):
+        """地震情報(確認用)"""
+        resp = urllib.request.urlopen('http://svir.jp/eew/data.json')
+        eew = json.loads(resp.read().decode('utf-8'))
         embed=discord.Embed(title="**震央の確からしさ0:不明1:P波/S波レベル越え、またはIPF法（1点）、または仮定震源要素の場合2:IPF法（2点）3:IPF法（3/4点）4:IPF法（5点以上）5:防災科研システム（4点以下、または精度情報なし）〈防災科研Hi-netデータ〉6:防災科研システム（5点以上）〈防災科研Hi-netデータ〉7:EPOS（海域〈観測網外〉）8:EPOS（陸域〈観測網内〉）9:予備**", description=eew['Body']['Earthquake']['Accuracy']['Epicenter[0]'])
         embed.add_field(name="**震源の確からしさ（気象庁内部システムで使用するため予告なく変わる場合がある）0:不明1:P波/S波レベル越え、またはIPF法（1点）、または仮定震源要素の場合2:IPF法（2点）3:IPF法（3/4点）4:IPF法（5点以上）5-8:予備9:従来法（IPF法を含む）の最終相当。推定震源・マグニチュードはこれ以降変化しない（ただしPLUM法により予想震度が今後変化する可能性はある）**", value=eew['Body']['Earthquake']['Accuracy']['Epicenter[1]'], inline=False)
         embed.add_field(name="**深さの確からしさ0:不明1:P波/S波レベル越え、またはIPF法（1点）、または仮定震源要素の場合2:IPF法（2点）3:IPF法（3/4点）4:IPF法（5点以上）5:防災科研システム（4点以下、または精度情報なし）〈防災科研Hi-netデータ〉6:防災科研システム（5点以上）〈防災科研Hi-netデータ〉7:EPOS（海域〈観測網外〉）8:EPOS（陸域〈観測網内〉）9:予備**", value=eew['Body']['Earthquake']['Accuracy']['Depth'], inline=False)
         embed.add_field(name="**マグニチュードの確からしさ0:不明1:未定義2:防災科研システム〈防災科研データ〉3:全点P相4:P相/全相混在5:全点全相6:EPOS7:未定義8:P波/S波レベル越え、または仮定震源要素の場合9:予備**", value=eew['Body']['Earthquake']['Accuracy']['MagnitudeCalculation'], inline=False) 
         embed.add_field(name="**マグニチュード計算使用観測点数（気象庁内部システムで使用するため予告なく変わる場合がある）0:不明1:P波/S波レベル越え、1点、または仮定震源要素の場合2:2点3:3点4:4点5:5点以上6-9:予備**", value=eew['Body']['Earthquake']['Accuracy']['NumberOfMagnitudeCalculation'], inline=False)
         await ctx.send(embed=embed)
-  
+ 
+    @commands.command()
+    async def eewop4(self, ctx):
+        """地震情報(確認用)"""
+        resp = urllib.request.urlopen('http://svir.jp/eew/data.json')
+        eew = json.loads(resp.read().decode('utf-8'))
         embed=discord.Embed(title="**マグニチュード不明時、「/./」となる**", description=eew['Body']['Earthquake']['Magnitude'], inline=False)
         embed.add_field(name="**最大予測震度1:震度12:震度23:震度34:震度45-:震度5弱5+:震度5強6-:震度6弱6+:震度6強7:震度7不明:不明時**", value=eew['Body']['Intensity']['MaxInt'], inline=False)
         embed.add_field(name="**テキストの予測最大震度**", value=eew['Body']['Intensity']['TextInt'], inline=False)
@@ -70,7 +86,13 @@ class eew(commands.Cog):
         embed.add_field(name="**最大予測震度変化0:ほとんど変化なし1:最大予測震度が1.0以上大きくなった2:最大予測震度が1.0以上小さくなった3-9:未定義**", value=eew['Body']['Intensity']['MaxIntChange'], inline=False)
         embed.add_field(name="**最大予測震度変化の理由0:変化なし1:主としてMが変化したため（1.0以上）2:主として震央位置が変化したため（10㎞以上）3:M及び震央位置が変化したため（1と2の複合条件）4:震源の深さが変化したため（上記のいずれにも当てはまらず、30㎞以上変化）5-8:未定義9:PLUM法による予想により変化したため**", value=eew['Body']['Intensity']['MaxIntChangeReason'], inline=False)
         await ctx.send(embed=embed)
-  
+ 
+    @commands.command()
+    async def eewop5(self, ctx):
+        """地震情報(確認用)"""
+        resp = urllib.request.urlopen('http://svir.jp/eew/data.json')
+        eew = json.loads(resp.read().decode('utf-8'))
+        #Head 
         embed=discord.Embed(title="**一次細分化地域名**", description=eew['Body']['Intensity']['Areas[]']['Name'], inline=False) 
         embed.add_field(name="**一次細分化地域コード**", value=eew['Body']['Intensity']['Areas[]']['Code'], inline=False)
         embed.add_field(name="**予報カテゴリー名の文字列表現「緊急地震速報（予報）」、「緊急地震速報（警報）」を記載。**", value=eew['Body']['Intensity']['Areas[]']['Kind']['Name'], inline=False)
