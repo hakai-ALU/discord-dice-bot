54aaf237067418cbb4dfb77677d0925576d2be85
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-18 22:29:19 +0900

    Update trpg.py

--- cogs/trpg.py
+++ cogs/trpg.py
@@ -1,13 +1,12 @@
 from discord.ext import commands # Bot Commands Frameworkのインポート
 import discord
 import asyncio
+import random
 
-# コグとして用いるクラスを定義。
 class TRPG(commands.Cog):
-    # trpgクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
+
     def __init__(self, bot):
         self.bot = bot
 
-# Bot本体側からコグを読み込む際に呼び出される関数。
 def setup(bot):
     bot.add_cog(TRPG(bot))
